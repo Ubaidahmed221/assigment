c7ccc7b5265f1feb2a78c660c3d193c42dc3bd0a
ubaid-12 <ubaidahmed221@gmail.com>
2022-07-07 02:45:48 -0700

    javascript

---
+++ assigment.js
@@ -0,0 +1,237 @@
+
+
+//         const first = prompt('enter your name');
+//         const  lastname = prompt('enter your  l.name');
+//         const fname = prompt('enter your fname');
+//        let age = prompt('enter your age');
+
+//       var  Data=[first, lastname,fname,++age]
+
+//         document.write(Data);
+
+// var tb1 = prompt('enter table')
+// var tb2 = prompt('enter table')
+// for(var i=1; i <= tb2; i++) {
+//     document.write( `${tb1} x ${i}= ${i*tb1}  <br>` )
+// }
+
+
+// let lot =() => {
+//     var tb1 = document.getElementById('start1').value
+//     var tb2 = document.getElementById('start2').value
+//     var tb3 = document.getElementById('start3').value
+
+//    tb3 = ++tb3
+
+//     for(var i=tb2; i <tb3; i++) {
+//         document.write(`${tb1} x ${i} = ${tb1*i}  <br>` )
+//     }
+// }
+
+
+// { city == 'karachi' ? alert(`welcome  ${city}`) : 'not welcome' }
+
+
+
+
+
+//  studentdata = [
+//         {
+//             f_name : 'ali',
+//             l_name : 'khan',
+//              city  : 'karachi',
+//              email  : 'ALI@gmail.com',
+//              phone  : 03161475624,
+//              country :  'pakistan'
+//         },
+
+//         {
+//                  f_name : 'usaman',
+//                 l_name : 'shah',
+//                 city  : 'lahore',
+//                 email  : 'usama@gmail.com',
+//                 phone  : 034556165,
+//                 country :  'pakistan'
+//         },
+
+//         {
+//             f_name : 'taha',
+//             l_name : 'khan',
+//                 city  : 'Islamabad',
+//                 email  : 'taha@gmail.com',
+//                 phone  : 03245605625,
+//                 country :  'pakistan'
+//         },
+
+//         {
+//                 f_name : 'Shakeel',
+//                 l_name : 'ahmed',
+//                 city  : 'pesahwar',
+//                 email  : 'shakeel@gmail.com',
+//                 phone  : 0331654623,
+//                 country :  'pakistan'
+//         },
+
+//         {
+//                 f_name : 'usman',
+//                 l_name : 'khan',
+//                 city  : 'punjab',
+//                 email  : 'usman@gmail.com',
+//                 phone  : 0353564562,
+//                 country :  'pakistan'
+//         },
+
+//         {
+//                 f_name : 'kamran',
+//                 l_name : 'khan',
+//                 city  : 'qutta',
+//                 email  : 'kamran@gmail.com',
+//                 phone  :  03265245632 ,
+//                 country :  'pakistan'
+//         },
+
+
+// ]
+// studentdata.map(i=>{
+//     // document.write(i.l_name)
+//     document.write(
+//         `<p>${i.f_name} ${i.l_name} ${i.city} ${i.email} ${i.phone} ${i.country}</p>
+//         `
+//     )
+// })
+
+
+
+
+//  var  Image =[ `<img src='image1.jpg'>  <img src='image2.jpg'>  <img src='image3.jpg'>` ]
+
+//  document.write(Image)
+
+//  let num =[10,20,30,40,50,60,70,80,90,100]
+// const findnumindex = num.find(i => i>40);
+// console.log(findnumindex)
+
+// var countries =['karachi', 'lahore','pesahawer' ,'islamabad']
+//  countries.map( i=> document.write(`${countries}`))
+
+
+
+
+// function show(){
+
+// var a =prompt('enter your Number')
+// var b=prompt('enter your Number')
+
+// let prompt1=[a]
+// let prompt2=[b]
+// prompt1.map(i=> document.write(i=`${prompt1*prompt2}`))
+// }
+
+// function show(){
+//  setTimeout(
+//      ()=>{console.log('hello')},1000
+//  );
+// const click = setInterval(
+//     () => { console.log('clear') }, 1000
+// );
+// function show() {
+//     clearInterval(click)
+// }}
+
+
+// var sum = prompt('enter your massege')
+// var sum2 = prompt('enter your massege')
+
+// let arr = ['sum' ,'sum2']
+// arr.map(i => document.write(i = `${'sum'+'' +'sum2'}`))
+
+
+// var Date = new Date()
+// document.write(Date.getDay())
+
+// setInterval(()=>{
+//     var date =new Date()
+//    console.log(date.toLocaleTimeString())
+// },1000)
+
+// var mypromise = new Promise((resolve,reject)=>{
+//     var uname = prompt('enter your name')
+//     var fname= prompt('enter your fname')
+//     if(uname =='any name', fname =='any fname'){
+//         resolve()
+//     }
+//     else{
+//         reject()
+//     }
+// })
+//    mypromise.then(
+//     function(){
+//         localStorage.setItem(JSON.stringify(mypromise))
+//     }
+// )}
+
+
+// function show(num1,num2){
+//     var total = num1 + num2
+//     return alert(total)
+// }
+
+// function show(num1,num2){
+//     var total = num1 - num2
+//     return alert(total)
+// }
+
+// function show(num1,num2){
+//     var total = num1 * num2
+//     return alert(total)
+// }
+
+// function show(num1,num2){
+//     var total = num1 / num2
+//     return alert(total)
+// }
+// var name = 'ubaid'
+// alert(name)
+
+// var student = {
+//     name  : 'ubaid',
+//     age : 17,
+//     country : 'pakistan',
+//     city : 'karachi',
+// }
+// alert(student.age)
+
+
+// var arre=[
+//     'ahmed' ,18, 'karachi'
+// ]
+// alert(age)
+// arre.push('most')
+//     alert(arre)
+
+    // arre.pop('most')
+    // alert(arre)
+
+    // arre.unshift('like')
+    // alert(arre)
+
+    // arre.shift('like')
+    // alert(arre)
+
+
+//     var arry=[20,30,50,80,40]
+//     arry.push(10)
+//     alert(arry)
+
+//     arry.pop(10)
+//     alert(arry)
+
+//     arry.unshift(10)
+//     alert(arry)
+
+//     arry.shift(10)
+//     alert(arry)
+
+//    
+
+
